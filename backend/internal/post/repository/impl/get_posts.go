526ffc6c8f457053cf42c0f4a6c25b31bdeb3afa
Yudistira Maulana <yudistir4maulana@gmail.com>
2023-06-01 22:30:06 +0700

    update get_posts handling query caption and userID

--- backend/internal/post/repository/impl/get_posts.go
+++ backend/internal/post/repository/impl/get_posts.go
@@ -17,6 +17,8 @@ func (r *postRepository) GetPosts(req dto.GetPostsReq) ([]dto.Post, error) {
 		query = query.Where("caption LIKE ? AND post_category = ? AND user_id = ?", "%"+req.Caption+"%", req.PostCategory, req.UserID)
 	} else if req.Caption != "" && req.PostCategory != "" {
 		query = query.Where("caption LIKE ? AND post_category = ?", "%"+req.Caption+"%", req.PostCategory)
+	} else if req.Caption != "" && req.UserID != uuid.Nil {
+		query = query.Where("caption LIKE ? AND user_id = ?", "%"+req.Caption+"%", req.UserID)
 	} else if req.Caption != "" {
 		query = query.Where("caption LIKE ?", "%"+req.Caption+"%")
 	} else if req.PostCategory != "" {
