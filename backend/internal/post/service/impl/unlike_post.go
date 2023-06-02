737dc78432b6579ca6713d2bfc4886cdbab54c3e
Yudistira Maulana <yudistir4maulana@gmail.com>
2023-06-02 09:59:27 +0700

    handle error post has deleted

--- backend/internal/post/service/impl/unlike_post.go
+++ backend/internal/post/service/impl/unlike_post.go
@@ -1,7 +1,17 @@
 package impl
 
-import "backend/pkg/dto"
+import (
+	"backend/pkg/dto"
+	customerrors "backend/pkg/errors"
+)
 
 func (s *postService) UnlikePost(req dto.PostAction) error {
+	_, err := s.repo.GetPostByID(req.PostID)
+	if err != nil {
+		if err == customerrors.ErrPostNotFound {
+			return customerrors.ErrPostHasBeenDeleted
+		}
+		return err
+	}
 	return s.repoLike.DeleteLike(req)
 }
